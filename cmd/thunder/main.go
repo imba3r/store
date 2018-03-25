960128353b9da35b0a5c72040d2af6dee38824ae
imba3r <imba3r@gmail.com>
2018-03-25 19:17:32 +0200

    Handler integrated..

--- cmd/thunder/main.go
+++ cmd/thunder/main.go
@@ -1,119 +1,13 @@
 package main
 
 import (
-	"github.com/imba3r/thunder"
 	"net/http"
-	"log"
 
-	"github.com/gorilla/websocket"
-	"encoding/json"
+	"github.com/imba3r/thunder"
 )
-
-type Operation int
-
-const (
-	Subscribe Operation = iota
-	Insert
-	Update
-	Delete
-	Snapshot
-)
-
-type Message struct {
-	Key       string          `json:"key"`
-	Operation Operation       `json:"operation,omitempty"`
-	Payload   json.RawMessage `json:"payload"`
-}
 
 func main() {
 	s := thunder.NewStore("/tmp/badger")
-
-	var upgrader = websocket.Upgrader{
-		ReadBufferSize:  1024,
-		WriteBufferSize: 1024,
-		CheckOrigin:     func(r *http.Request) bool { return true },
-	}
-
-	http.HandleFunc("/thunder", func(w http.ResponseWriter, r *http.Request) {
-		conn, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		subscriptions := make(map[string]chan []byte)
-		conn.SetCloseHandler(func(code int, text string) error {
-			for key, channel := range subscriptions {
-				s.Unsubscribe(key, channel)
-			}
-			return nil;
-		})
-
-		for {
-			msgType, msg, err := conn.ReadMessage()
-			if err != nil {
-				log.Println("[ERR] ReadMessage", err)
-				return
-			}
-			if msgType != websocket.TextMessage {
-				log.Println("[ERR] Invalid message type")
-				continue
-			}
-
-			var m Message
-			err = json.Unmarshal(msg, &m)
-			if err != nil {
-				log.Println("[ERR] Unmarshal", err)
-				continue
-			}
-
-			switch m.Operation {
-			case Subscribe:
-				subscriptions[m.Key] = s.Subscribe(m.Key)
-				go listen(m.Key, subscriptions[m.Key], conn)
-			case Update:
-				s.Update(m.Key, m.Payload)
-			case Insert:
-				s.Insert(m.Key, m.Payload)
-			case Delete:
-				s.Delete(m.Key)
-			case Snapshot:
-				data, _ := s.Read(m.Key)
-				writeMessage(conn, createAnswer(m.Key, data))
-			}
-		}
-	})
+	http.HandleFunc("/thunder", s.HandlerFunc())
 	http.ListenAndServe(":3000", nil)
-}
-
-func listen(key string, channel chan []byte, conn *websocket.Conn) {
-	log.Println("Listening....")
-	for {
-		select {
-		case m, ok := <-channel:
-			if !ok {
-				log.Println("Stopped listening..")
-				return
-			}
-			writeMessage(conn, createAnswer(key, m))
-		}
-	}
-}
-
-func writeMessage(conn *websocket.Conn, message []byte) {
-	err := conn.WriteMessage(websocket.TextMessage, message)
-	if err != nil {
-		log.Println("[ERR] listen().WriteMessage", err)
-	}
-}
-
-func createAnswer(key string, data []byte) []byte {
-	answer := &Message{
-		Key:     key,
-		Payload: data,
-	}
-	data, err := json.Marshal(answer)
-	if err != nil {
-		log.Println("[ERR] createAnswer().Marshal", err)
-	}
-	return data
 }
--- thunder.go
+++ thunder.go
@@ -2,13 +2,36 @@ package thunder
 
 import (
 	"log"
+	"encoding/json"
+	"net/http"
+	"sync"
 
 	"github.com/dgraph-io/badger"
+	"github.com/gorilla/websocket"
 )
+
+type WebsocketOperation int
+
+const (
+	Subscribe WebsocketOperation = iota
+	Insert
+	Update
+	Delete
+	Snapshot
+)
+
+type Message struct {
+	Key       string             `json:"key"`
+	Operation WebsocketOperation `json:"operation,omitempty"`
+	Payload   json.RawMessage    `json:"payload"`
+}
 
 type Store struct {
 	db  *badger.DB
 	reg *registry
+
+	mutex    sync.Mutex
+	upgrader *websocket.Upgrader
 }
 
 func (s *Store) Close() {
@@ -26,6 +49,11 @@ func NewStore(path string) *Store {
 	return &Store{
 		db:  db,
 		reg: Newregistry(),
+		upgrader: &websocket.Upgrader{
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+			CheckOrigin:     func(r *http.Request) bool { return true },
+		},
 	}
 }
 
@@ -34,7 +62,6 @@ func (s *Store) Insert(key string, data []byte) error {
 		return txn.Set([]byte(key), data)
 	})
 	if err == nil {
-		log.Println("[INSERT] Publishing", key, string(data))
 		s.reg.publish(key, data)
 	}
 	return err
@@ -45,7 +72,6 @@ func (s *Store) Update(key string, data []byte) error {
 		return txn.Set([]byte(key), data)
 	})
 	if err == nil {
-		log.Println("[UPDATE] Publishing", key, string(data))
 		s.reg.publish(key, data)
 	}
 	return err
@@ -56,7 +82,6 @@ func (s *Store) Delete(key string) error {
 		return txn.Delete([]byte(key))
 	})
 	if err == nil {
-		log.Println("[DELETE] Publishing", key)
 		s.reg.publish(key, nil)
 	}
 	return err
@@ -91,16 +116,101 @@ func (s *Store) Unsubscribe(key string, channel chan []byte) {
 	s.reg.unsubscribe(key, channel)
 }
 
-type operation int
+func (s *Store) HandlerFunc() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		conn, err := s.upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		subscriptions := make(map[string]chan []byte)
+		conn.SetCloseHandler(func(code int, text string) error {
+			for key, channel := range subscriptions {
+				s.Unsubscribe(key, channel)
+			}
+			return nil;
+		})
+
+		for {
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				log.Println("[ERR] websocket.Conn.ReadMessage", err)
+				return
+			}
+			if msgType != websocket.TextMessage {
+				log.Println("[ERR] messageType must be websocket.TextMessage")
+				continue
+			}
+
+			var m Message
+			err = json.Unmarshal(msg, &m)
+			if err != nil {
+				log.Println("[ERR] json.Unmarshal", err)
+				continue
+			}
+
+			switch m.Operation {
+			case Subscribe:
+				subscriptions[m.Key] = s.Subscribe(m.Key)
+				go s.listen(m.Key, subscriptions[m.Key], conn)
+			case Update:
+				s.Update(m.Key, m.Payload)
+			case Insert:
+				s.Insert(m.Key, m.Payload)
+			case Delete:
+				s.Delete(m.Key)
+			case Snapshot:
+				data, _ := s.Read(m.Key)
+				s.writeMessage(conn, createAnswer(m.Key, data))
+			}
+		}
+	}
+}
+
+func (s *Store) listen(key string, channel chan []byte, conn *websocket.Conn) {
+	for {
+		select {
+		case m, ok := <-channel:
+			if !ok {
+				return
+			}
+			s.writeMessage(conn, createAnswer(key, m))
+		}
+	}
+}
+
+func (s *Store) writeMessage(conn *websocket.Conn, message []byte) {
+	defer s.mutex.Unlock()
+	s.mutex.Lock()
+
+	err := conn.WriteMessage(websocket.TextMessage, message)
+	if err != nil {
+		log.Println("[ERR] websocket.Conn.WriteMessage", err)
+	}
+}
+
+func createAnswer(key string, data []byte) []byte {
+	answer := &Message{
+		Key:     key,
+		Payload: data,
+	}
+	data, err := json.Marshal(answer)
+	if err != nil {
+		log.Println("[ERR] json.Marshal", err)
+	}
+	return data
+}
+
+type registryOperation int
 
 const (
-	sub   operation = iota
+	sub   registryOperation = iota
 	pub
 	unsub
 )
 
 type cmd struct {
-	op  operation
+	op  registryOperation
 	key string
 
 	channel chan []byte
@@ -144,13 +254,10 @@ func (r *registry) start() {
 	for cmd := range r.cmdChan {
 		switch cmd.op {
 		case pub:
-			log.Println("[REG] Handling pub")
 			r.doPublish(cmd.key, cmd.data)
 		case sub:
-			log.Println("[REG] Handling sub")
 			r.doSubscribe(cmd.key, cmd.channel)
 		case unsub:
-			log.Println("[REG] Handling unsub")
 			r.doUnsubscribe(cmd.key, cmd.channel)
 		}
 	}
@@ -176,7 +283,6 @@ func (r *registry) doSubscribe(key string, channel chan []byte) {
 			key:         key,
 			subscribers: []chan []byte{channel},
 		}
-		log.Println("[SUB]", key, "didn't exist yet.")
 	} else {
 		e.subscribers = append(e.subscribers, channel)
 	}
